b8ed5ec0a0f78f0f7c2817173b059fce42aaa158
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-02-25 18:56:11 -0300

    Add link to thumbnail image.

--- s3thumbnails/images/admin.py
+++ s3thumbnails/images/admin.py
@@ -13,13 +13,19 @@ from .models import Image
 
 
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('id', 'link_image')
+    list_display = ('id', 'link_image', 'link_thumbnail')
 
     def link_image(self, obj):
         return u"<a href='{0}'>{0}</a>".format(obj.image.url)
 
     link_image.allow_tags = True
     link_image.short_description = u'Image'
+
+    def link_thumbnail(self, obj):
+        return u"<a href='{0}'>{0}</a>".format(obj.thumbnail().url)
+
+    link_thumbnail.allow_tags = True
+    link_thumbnail.short_description = u'Thumbnail'
 
 
 admin.site.register(Image, ImageAdmin)
